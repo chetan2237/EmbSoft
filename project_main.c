19ad6e05842a896821207f14ccfb958c4c105077
chetan2237 <pidugunagachetan2000@gmail.com>
2021-04-24 16:34:04 +0530

    Done

--- project_main.c
+++ project_main.c
@@ -1,7 +1,7 @@
 /**
  * @file project_main.c
  * @author Chetan ()
- * @brief Project to Blink an LED at 1000ms ON 
+ * @brief Project to Blink an LED at 1000ms ON when the heater is on.
  * @version 0.1
  * @date 2021-04-23
  * 
@@ -43,8 +43,8 @@ void change_led_state(uint8_t state)
  * @brief Main function where the code execution starts
  * 
  * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+* @note  if pin 0 of port C is high then pin 4 of port B is high
+* @note if above condition is not true then pin 4 of port B remain constant
  */
 int main(void)
 {
