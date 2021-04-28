1a15e04c01123656cdb07bd771d815f462234bfe
chetan2237 <pidugunagachetan2000@gmail.com>
2021-04-28 17:55:49 +0530

    Done

---
+++ inc/Activity2.h
@@ -0,0 +1,29 @@
+/*
+ */
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY_H_
+#include <avr/io.h>
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+/*
+ * ADCexample.c
+ *
+ * Created: 4/13/2013 5:22:48 PM
+ *  Author: SAM
+ */
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include<util/delay.h>
+#include <avr/interrupt.h>
+
+void InitADC();
+
+
+#endif /** __Activity_H_ */
